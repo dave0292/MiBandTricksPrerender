ada0dc80dea72e04edffbfc665ab7d9179bfbf12
Dave0292 <dave0292@gmail.com>
2022-01-12 08:55:31 +0100

    Initial commit of MiBandTricksPrerender

---
+++ MiBandTricksBlazor/Prerender/PrerenderScript.ps1
@@ -0,0 +1,14 @@
+If(Test-Path .\Prerender\output)
+{
+    Remove-Item -Path .\Prerender\output -Recurse
+}
+ 
+dotnet publish .\Client\BlazorWasmPrerender.csproj -c Release -o Prerender/output --nologo
+Push-Location .\Prerender
+npx react-snap
+Get-ChildItem ".\output\wwwroot\*.html" -Recurse | ForEach-Object { 
+    $HtmlFileContent = (Get-Content -Path $_.FullName -Raw);
+    $HtmlFileContent = $HtmlFileContent.Replace('<script type="text/javascript">var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;</script><script src="_framework/dotnet.5.0.1.js" defer="" integrity="sha256-SWZOE2EsCqc/7dPgJrcFqUvVvdeJ9cipeZ2NFMC9v2s=" crossorigin="anonymous"></script>','')
+    Set-Content -Path $_.FullName -Value $HtmlFileContent
+}
+Pop-Location
---
+++ MiBandTricksBlazor/Shared/GoogleAdSense/DisplayInArticleAdConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiBandTricksBlazor.Shared.GoogleAdSense
+{
+    public class DisplayInArticleAdConfiguration
+    {
+        public string AdClient { get; set; }
+        public string AdSlot { get; set; }
+    }
+}
